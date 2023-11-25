7521b1e405dccdfa0a62a819dc7c4a50661bbe73
Thomas Misch <ttmisch@gmail.com>
2023-11-25 19:31:07 +0100

    Added busybox web server inline, security group and ingress

--- playground/main.tf
+++ playground/main.tf
@@ -5,8 +5,41 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World!" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+  
+  user_data_replace_on_change = true
+  
   tags = {
     Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+} 
+
+resource "aws_vpc_security_group_ingress_rule" "example" {
+  security_group_id = aws_security_group.instance.id
+
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 8080
+  to_port = 8080
+}
+
+
+
+
+
+
+
+
+
+
 
